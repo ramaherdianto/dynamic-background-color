1628e0e12421e4808a07e60ec5d19c510b572be9
ramaherdianto <ramadhanherdianto35@gmail.com>
2023-01-30 15:03:37 +0700

    feat: update

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const changeColor = document.querySelector('#change-color');
 
 const setBackgroundColor = (color) => {
     const colorName = color.toLowerCase();
-    bgTarget.className = `bg-[${colorName}] p-5 max-w-2xl h-[200px] flex items-center justify-center rounded-md border-[5px] border-pink-700`;
+    bgTarget.className = `bg-[${colorName}] transition-all duration-[0.5s] ease-linear p-5 max-w-2xl h-[200px] flex items-center justify-center rounded-md border-[2px] border-pink-500`;
 };
 
 changeColor.addEventListener('click', function (e) {
